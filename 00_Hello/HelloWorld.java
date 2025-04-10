2b330ba8393e2269f59b0ae5fb0ea58e31dc112f
Rachit Garg <rachitgarg900@gmail.com>
2025-04-10 03:06:38 +0200

    Create HelloWorld.java

---
+++ 00_Hello/HelloWorld.java
@@ -0,0 +1,11 @@
+/* *****************************************************************************
+ *  Name:              Rachit Garg
+ *  Coursera User ID:  123456
+ *  Last modified:     April 09, 2025
+ **************************************************************************** */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
